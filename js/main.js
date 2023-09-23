982306c80238dbad308e3d5c9a83182c501721f8
Alexey Svistunov <xdayx53@gmail.com>
2023-09-23 14:44:01 +0300

    added js for the project

--- js/main.js
+++ js/main.js
@@ -1,17 +1,85 @@
 const chooseColor = document.querySelectorAll('.choose-color__btn');
 const contentItem = document.querySelectorAll('.content-item');
 
+
+
 chooseColor.forEach(function(element){
   element.addEventListener('click', open);
 });
 
-function open(event) {
+function open(event){
   const target = event.currentTarget;
+  const button = target.dataset.button;
+  const contentActive = document.querySelectorAll(`.${button}`);
 
-  chooseColor.forEach(function(item) {
+  chooseColor.forEach(function(item){
     item.classList.remove('choose-color__btn--active');
-  });  
+  });
 
   target.classList.add('choose-color__btn--active');
 
+  contentItem.forEach(function(item){
+    item.classList.remove('content-item--active');
+
+  });
+
+  contentActive.forEach(function(item){
+    item.classList.add('content-item--active');
+  });
+
+
 }
+
+// chooseColor.forEach(el =>{
+//   el.addEventListener('click', event =>{
+//     const self = event.currentTarget;
+//     const btn = self.dataset.button;
+//     const contentActive = document.querySelectorAll(`.${btn}`);
+//     console.log(btn);
+//     console.log(contentActive);
+    
+//     chooseColor.forEach(item =>{
+//       item.classList.remove('choose-color__btn--active');
+      
+//     });
+//     el.classList.add('choose-color__btn--active');
+
+//   });
+
+  
+// });
+
+
+// const chooseColor = document.querySelectorAll('.choose-color__btn');
+// const contentItem = document.querySelectorAll('.content-item');
+
+
+
+// chooseColor.forEach(function(element){
+//   element.addEventListener('click', open);
+// });
+
+// function open(event){
+//   const target = event.currentTarget;
+//   const btnData = target.dataset.button;
+  
+  
+//   const contentActive = document.querySelectorAll(`.${btnData }`);
+
+//   chooseColor.forEach(item =>{
+//     item.classList.remove('choose-color__btn--active');
+//   });
+
+//   target.classList.add('choose-color__btn--active');
+
+//   contentItem.forEach(item =>{
+//     item.classList.remove('content-item--active');
+
+//   });
+
+//   contentActive.forEach(item =>{
+//     item.classList.add('content-item--active');
+//   });
+
+
+// }
